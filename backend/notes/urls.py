77fbce31a22b1dde1d06b3fbeaf410da076d34c6
MilenHadzhiev <milen10hadzhiev@gmail.com>
2021-03-03 00:27:56 +0200

    Add lesson to notebook

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -4,7 +4,7 @@ from notes.views import NoteCreate, NotesList, NotePage, NoteEdit, NoteDelete
 
 urlpatterns = [
     path('create/', NoteCreate.as_view(), name='create note'),
-    path('create/<int:pk>/', NoteCreate.as_view(), name='create note for tutorial'),
+    path('create/<int:pk>/', NoteCreate.as_view(), name='create note for lesson'),
     path('all/', NotesList.as_view(), name='my notes'),
     path('<int:pk>/', NotePage.as_view(), name='current note'),
     path('edit/<int:pk>/', NoteEdit.as_view(), name='edit note'),
