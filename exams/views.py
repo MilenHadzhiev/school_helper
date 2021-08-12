74d7ba1e23013241be7377ed998c1a659d9cc2a6
MilenHadzhiev <milen10hadzhiev@gmail.com>
2021-08-12 16:24:04 +0300

    Update exam logic and add comments

--- exams/views.py
+++ exams/views.py
@@ -1,15 +1,13 @@
 from os.path import join
 
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
-# Create your views here.
 from django.views.generic import ListView, CreateView
 
 from exams.models import Exam
-from questions.models import Question, Answer
+from questions.models import Question
 from results.models import Result
-from results.views import exam_result
 
 
 class ExamCreate(CreateView):
@@ -48,14 +46,10 @@ def exam_data_view(request, pk):
 
 def exam_save_view(request, pk):
     if request.is_ajax():
-        questions = []
-        data = request.POST
-        data_ = dict(data.lists())
-        data_.pop('csrfmiddlewaretoken')
-        print(data_)
-        for k in data_.keys():
-            question = Question.objects.get(text=k)
-            questions.append(question)
+        questions = []  # processing the input information
+        data = request.POST  # after the user has submitted the exam
+        data_ = dict(data.lists())  # converting the querydict into normal dict
+        data_.pop('csrfmiddlewaretoken')  # removing the csrf token; only question-answer pairs are left in data_
 
         user = request.user
         exam = Exam.objects.get(pk=pk)
@@ -64,32 +58,43 @@ def exam_save_view(request, pk):
         results = []
         correct_answer = None
 
-        for q in questions:
-            answer_selected = data_.get(str(q))[0]
-
-            if answer_selected != '':
-                question_answers = Answer.objects.filter(question=q)
-                for a in question_answers:
-                    if answer_selected == a.text:
-                        if a.correct_answer:
-                            score += 1
-                            correct_answer = a.text
+        for (q, a) in data_.items():  # iterating through the question-answer pairs
+            if a[0] != '':  # a is a list of answers containing one string
+                question = Question.objects.get(text=q)
+                question_answers = question.get_answers()   # getting all answers tied to this question object
+                for answer in question_answers:
+                    if a[0] == answer.text:  # checking if the answer selected by the user is the correct one
+                        if answer.correct_answer:  # a[0] is the answer selected by the user
+                            score += 1  # answer is one of the possible answers
+                            correct_answer = answer.text
                             break
                     else:
-                        if a.correct_answer:
-                            correct_answer = a.text
+                        if answer.correct_answer:
+                            correct_answer = answer.text
                             break
+
                 results.append(
-                    {str(q): {
+                    {q: {
                         'correct': correct_answer,
-                        'selected': answer_selected
-                    }}
+                        'selected': a[0]
+                    }
+                    }
                 )
             else:
-                results.append({str(q): 'Не е отговорен'})
+                results.append({q: 'Не е отговорен'})
 
-        score_ = score * multiplier
+        score_ = round(score * multiplier, 2)
         result = Result.objects.create(exam=exam, user=user, score=score_)
+
+        grade = 2
+        if score_ > exam.required_score_to_pass:
+            grade = 3
+        if score_ > exam.required_score_four:
+            grade = 4
+        if score_ > exam.required_score_five:
+            grade = 5
+        if score_ > exam.required_score_six:
+            grade = 6
 
         if score_ >= exam.required_score_to_pass:
             url = 'http://' + request.get_host()
@@ -98,12 +103,15 @@ def exam_save_view(request, pk):
                 {'passed': True,
                  'score': score_,
                  'results': results,
+                 'grade': grade,
                  'redirect_url': redirect_url})
         else:
             url = 'http://' + request.get_host()
             redirect_url = join(url, 'exams', 'results', str(result.id))
             return JsonResponse(
-                {'passed': True,
+                {'passed': False,
                  'score': score_,
                  'results': results,
-                 'redirect_url': redirect_url})
+                 'grade': grade,
+                 'redirect_url': redirect_url}
+            )
--- static/exams/exam.js
+++ static/exams/exam.js
@@ -1,6 +1,8 @@
 const url = window.location.href
-
 const examBox = document.getElementById('exam-box')
+const scoreBox = document.getElementById('score-box')
+const resultBox = document.getElementById('result-box')
+
 $.ajax({
     type: 'GET',
     url: `${url}data`,
@@ -54,9 +56,43 @@ const sendData = () => {
         url: `${url}save/`,
         data: data,
         success: function (response) {
-            const redirect_url = response['redirect_url']
-            console.log(redirect_url)
-            window.location.replace(redirect_url)
+            const results = response.results
+
+            examForm.classList.add('not-visible')
+
+            scoreBox.classList.add('bg-info')
+
+            scoreBox.classList.add(...['container', 'p-3', 'text-light', 'h3'])
+            scoreBox.innerHTML += `Твоят резултат е ${response.score}%\nОценка: ${response.grade}`
+            console.log(response.grade)
+            results.forEach(r => {
+
+                const resultDiv = document.createElement('div')
+                resultDiv.style.borderRadius = '5px'
+                resultDiv.style.paddingTop = '5px'
+
+                for (const [question, answer] of Object.entries(r)) {
+                    resultDiv.innerHTML += question
+                    const divClasses = ['container', 'p-3', 'text-light', 'h3']
+                    resultDiv.classList.add(...divClasses)
+
+                    if (answer === 'Не е отговорен') {
+                        resultDiv.innerHTML += `<br>Не е отговорен`
+                        resultDiv.classList.add('bg-danger')
+                    } else {
+                        const answered = answer['selected']
+                        const correctAnswer = answer['correct']
+                        if (answered === correctAnswer) {
+                            resultDiv.classList.add('bg-success')
+                            resultDiv.innerHTML += `<br>Правилен отговор: ${answered}`
+                        } else {
+                            resultDiv.classList.add('bg-danger')
+                            resultDiv.innerHTML += `<br>Избран отговор: ${answered}<br>Верен отговор: ${correctAnswer}`
+                        }
+                    }
+                }
+                resultBox.append(resultDiv)
+            })
         },
         error: function (error) {
             console.log(error)
